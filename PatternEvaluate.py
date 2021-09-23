369faaf1c1b45d80481a23c452acc1328363a24a
Samiha Shimmi <samiha_14@yahoo.com>
2021-09-22 23:11:44 -0500

    pattern2 evaluation on progress

--- PatternEvaluate.py
+++ PatternEvaluate.py
@@ -6,10 +6,17 @@ import generateXMLLinebyLine
 from xml.etree import ElementTree
 
 parentRoot = r"H:\Research\TestEvolution\TE\XMLHolders\\P2"
-sourceCodePathv2= "H:\Research\TestEvolution\DataAnalysis\mug-mug-root-5.2\mug-mug-root-5.2\mug\src"
+sourceCodePathv2= "H:\Research\TestEvolution\DataAnalysis\mug-mug-root-5.2\mug-mug-root-5.2\mug\src\main"
+deprecatedMethodList = list()
 
 
 loopC = 0
+
+def parseFunctionName(xmlFileName):
+
+    root = ElementTree.parse(xmlFileName).getroot()
+    name = root.find("name").text
+    return name
 
 def javaToXML(nameWithoutPath,fileNameFull,outPath):
 
@@ -35,33 +42,96 @@ def javaToXMLPreprocessing(fileNameList):
         parseName = nameParsed[0].rpartition("\\")
         tuk = name.rpartition(sourceCodePathv2)
         outFolder = (tuk[2].rpartition(".java"))
-        tt = outFolder[0].rpartition("\\")
         if not os.path.exists(parentRoot + outFolder[0]):
             os.makedirs(parentRoot + outFolder[0], 0o777)
         javaToXML(parseName[2] + ".xml", name, parentRoot + outFolder[0] + r"\\")
 
+def removeExtraBracket(xmlFile):
+    #print("parse node xml file")
+    #print(xmlFile)
+
+    with open(xmlFile, 'rb+') as f:
+        f.seek(f.tell() - 1, 2)  # f.seek(0,2) is legal for last char in both python 2 and 3 though
+        chu = f.read()
+        my_str_as_bytes1 = str.encode("}")
+        my_str_as_bytes2 = str.encode(")")
+        if my_str_as_bytes1 == chu or my_str_as_bytes2 == chu:
+            f.seek(-1, os.SEEK_END)
+            f.truncate()
 
 def parseNode(xmlFile):
-    print("-----------------------------")
+    #print("parse node xml file")
+    '''
     print(xmlFile)
     try:
         root = ElementTree.parse(xmlFile).getroot()
     except Exception as e:
         print(e)
-    for item in root.iter("function"):
-        print("*******************************************************************")
-        print("function")
+    try:
+        for item in root.iter(elementName):
+            print(ElementTree.tostring(item, encoding='unicode', method='xml'))
+    except Exception as e:
+        print("error parsing")
+    '''
+    with open(xmlFile,"rt") as f:
+        for line in f:
+            if "<annotation>@<name>Deprecated" in line:
+                print("deprecated found")
+                print(parseFunctionName(xmlFile))
 
+
+def parseFunction(xmlFile,elementName,functionStorePath):
+    print("location")
+    print(functionStorePath)
+    try:
+        root = ElementTree.parse(xmlFile).getroot()
+    except Exception as e:
+        print(e)
+    elements = list()
+    elementCount = 0
+    for item in root.iter(elementName):
+        #print(ElementTree.tostring(item, encoding='unicode', method='xml'))
+        #print("function count")
+        file = open(functionStorePath+r"\\"+elementCount.__str__()+".xml","wt")
+        file.write(ElementTree.tostring(item, encoding='unicode', method='xml'))
+        elementCount += 1
+        elements.append(ElementTree.tostring(item, encoding='unicode', method='xml'))
+    return elements
 
 def XMLParser(XMLfileNameList):
+    count = 0
     for item in XMLfileNameList:
         itemTemp = r"H:\Research\TestEvolution\TE\XMLHolders\P2\xmlTemp\temp.xml"
+
         with open(item,"rt") as fin:
             with open(itemTemp,"wt") as fout:
                 for line in fin:
                     fout.write(line.replace('http://www.srcML.org/srcML/src', ''))
+            print("-----------------------------")
             print(item)
-            parseNode(itemTemp)
+            functionStorePath = item.rpartition("\\")[0]
+            elementsFetched = parseFunction(itemTemp,"function",functionStorePath)
+            #print(elementsFetched)
+            for element in elementsFetched:
+                if "Deprecated" in element:
+                    count += 1
+            findDeprecated(functionStorePath)
+    print("total deprecated found ")
+    print(count)
+
+def findDeprecated(functionStorePath):
+    print("printing the xml created")
+    listOfFiles = fileNames(functionStorePath,".xml")
+
+    for item in listOfFiles:
+        if (item.rpartition("\\")[2].rpartition(".xml")[0].isnumeric()) == False:
+            listOfFiles.remove(item)
+    print(listOfFiles)
+
+    for item in listOfFiles:
+        removeExtraBracket(item)
+        parseNode(item)
+
 
 def do():
     fileNameList = fileNames(sourceCodePathv2, ".java")
@@ -70,5 +140,6 @@ def do():
     XMLfileNameList = fileNames(parentRoot, ".xml")
 
     XMLParser(XMLfileNameList)
+
 
 do()
